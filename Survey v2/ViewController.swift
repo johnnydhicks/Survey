a952de4637f3b65ec33bf923e081dd270d2b1663
johnnydhicks <johnnydhicks@users.noreply.github.com>
2017-10-05 09:56:41 -0600

    Initial Commit

---
+++ Survey v2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Survey v2
+//
+//  Created by Johnny Hicks on 10/5/17.
+//  Copyright © 2017 Hicks Enterprises. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
